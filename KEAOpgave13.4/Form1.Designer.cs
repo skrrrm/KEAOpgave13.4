c12c9c2f9dc168e9cd1ef1d5041f0ac73b3ed5c6
Alex Skram Jensen <asj@tutamail.com>
2020-11-23 10:42:36 +0100

    Add project files.

---
+++ KEAOpgave13.4/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace KEAOpgave13._4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelHeight = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxHeight = new System.Windows.Forms.TextBox();
+            this.textBoxWidth = new System.Windows.Forms.TextBox();
+            this.textBoxLength = new System.Windows.Forms.TextBox();
+            this.textBoxVolume = new System.Windows.Forms.TextBox();
+            this.textBoxSurface = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelHeight
+            // 
+            this.labelHeight.AutoSize = true;
+            this.labelHeight.Location = new System.Drawing.Point(40, 22);
+            this.labelHeight.Name = "labelHeight";
+            this.labelHeight.Size = new System.Drawing.Size(53, 17);
+            this.labelHeight.TabIndex = 0;
+            this.labelHeight.Text = "Height:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Width";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Length";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 326);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Volume:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 377);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Surface area:";
+            // 
+            // textBoxHeight
+            // 
+            this.textBoxHeight.Location = new System.Drawing.Point(146, 22);
+            this.textBoxHeight.Name = "textBoxHeight";
+            this.textBoxHeight.Size = new System.Drawing.Size(123, 22);
+            this.textBoxHeight.TabIndex = 5;
+            // 
+            // textBoxWidth
+            // 
+            this.textBoxWidth.Location = new System.Drawing.Point(146, 91);
+            this.textBoxWidth.Name = "textBoxWidth";
+            this.textBoxWidth.Size = new System.Drawing.Size(123, 22);
+            this.textBoxWidth.TabIndex = 6;
+            // 
+            // textBoxLength
+            // 
+            this.textBoxLength.Location = new System.Drawing.Point(146, 169);
+            this.textBoxLength.Name = "textBoxLength";
+            this.textBoxLength.Size = new System.Drawing.Size(123, 22);
+            this.textBoxLength.TabIndex = 7;
+            // 
+            // textBoxVolume
+            // 
+            this.textBoxVolume.Location = new System.Drawing.Point(146, 321);
+            this.textBoxVolume.Name = "textBoxVolume";
+            this.textBoxVolume.Size = new System.Drawing.Size(123, 22);
+            this.textBoxVolume.TabIndex = 8;
+            // 
+            // textBoxSurface
+            // 
+            this.textBoxSurface.Location = new System.Drawing.Point(146, 372);
+            this.textBoxSurface.Name = "textBoxSurface";
+            this.textBoxSurface.Size = new System.Drawing.Size(123, 22);
+            this.textBoxSurface.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(146, 254);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 30);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelError
+            // 
+            this.labelError.AutoSize = true;
+            this.labelError.Location = new System.Drawing.Point(43, 222);
+            this.labelError.Name = "labelError";
+            this.labelError.Size = new System.Drawing.Size(0, 17);
+            this.labelError.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelError);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxSurface);
+            this.Controls.Add(this.textBoxVolume);
+            this.Controls.Add(this.textBoxLength);
+            this.Controls.Add(this.textBoxWidth);
+            this.Controls.Add(this.textBoxHeight);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelHeight);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelHeight;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxHeight;
+        private System.Windows.Forms.TextBox textBoxWidth;
+        private System.Windows.Forms.TextBox textBoxLength;
+        private System.Windows.Forms.TextBox textBoxVolume;
+        private System.Windows.Forms.TextBox textBoxSurface;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelError;
+    }
+}
+
---
+++ KEAOpgave13.4/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KEAOpgave13._4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // OPGAVE 13.4
+            //Box myBox = new Box(); 
+
+            // OPGAVE 13.6
+            Box myBox = new Box(Convert.ToDouble(textBoxHeight.Text), Convert.ToDouble(textBoxWidth.Text), Convert.ToDouble(textBoxLength.Text));
+            try
+            {
+                // OPGAVE 13.4
+                // Converting input in textboxes to double
+                //myBox.Height = Convert.ToDouble(textBoxHeight.Text);
+                //myBox.Width = Convert.ToDouble(textBoxWidth.Text);
+                //myBox.Length = Convert.ToDouble(textBoxLength.Text);
+
+            }
+            catch
+            {
+
+                labelError.Text = "Numbers only!";
+            }
+            textBoxVolume.Text = Convert.ToString(myBox.Volume());
+            textBoxSurface.Text = Convert.ToString(myBox.SurfaceArea());
+
+        }
+
+        class Box
+        {
+            // Fields (are private)
+            private double height = -1;
+            private double width = -1;
+            private double length = -1;
+
+
+            // Properties (are public)
+            public double Height
+            {
+                get
+                {
+                    return height;
+                }
+                set
+                {
+                    if (value > 0)
+                        height = value;
+                    else
+                        height = 10;
+                }
+            }
+            public double Width
+            {
+                get
+                {
+                    return width;
+                }
+                set
+                {
+                    if (value > 0)
+                        width = value;
+                    else
+                        width = 10;
+                }
+            }
+
+            public double Length
+            {
+                get
+                {
+                    return length;
+                }
+                set
+                {
+                    if (value > 0)
+                        length = value;
+                    else
+                        length = 10;
+                }
+            }
+
+            // OPGAVE 13.4
+            // Constructor - same name as the class
+            public Box()
+            {
+                Volume();
+                SurfaceArea();
+            }
+
+            // OPGAVE 13.6
+            // Constructor with properties
+            public Box(double height, double width, double length)
+            {
+                this.Height = height;
+                this.Width = width;
+                this.Length = length;
+                
+                Volume();
+                SurfaceArea();
+
+            }
+            /// <summary>
+            /// Method for calculating the volume of a box
+            /// </summary>
+            /// <returns>v (volume of box)</returns>
+            public double Volume()
+            {
+                double v = this.height * this.width * this.length;
+                return v;
+            }
+
+            /// <summary>
+            /// Method for calculating the surface area of a box
+            /// </summary>
+            /// <returns>area</returns>
+            public double SurfaceArea()
+            {
+                double area = 2 * (this.length * this.height + this.height * this.width + this.width * this.length);
+                return area;
+            }
+        }
+    }
+}
